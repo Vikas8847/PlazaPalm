c181ba818681a50b548ea06b39d7401d3f5e37e9
Khem singh <sfs.khem22@gmail.com>
2023-01-24 19:22:14 +0530

    24-01-23 Evening  code update2 Calendar Screen Change

--- app/src/main/java/com/example/plazapalm/views/myprofile/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/plazapalm/views/myprofile/calendar/CalendarFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnCalendarPageChangeListener
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener
 import com.example.plazapalm.R
 import com.example.plazapalm.databinding.CalendarFragmentBinding
@@ -39,6 +40,7 @@ class CalendarFragment : Fragment(R.layout.calendar_fragment) {
         //calendarClick()
         getBundleData()
         calendarClick()
+
         binding?.vm = viewModel
     }
 
@@ -69,18 +71,22 @@ class CalendarFragment : Fragment(R.layout.calendar_fragment) {
     @SuppressLint("ResourceType")
     private fun calendarClick() {
 
-        val calendars: List<Calendar> = ArrayList()
+        val calandar = Calendar.getInstance()
+        var month = 1
 
         viewModel.SeletedDate.observe(requireActivity()){
-
             Log.e("SDSDQQWq" , it.toString())
         }
 
 
+
+/*
         binding?.clCalendar?.setOnDayClickListener(object : OnDayClickListener {
             override fun onDayClick(eventDay: EventDay) {
                 val clickedDayCalendar = eventDay.calendar
-               /* val date = (dayofMonth.toString() + "-" + (month + 1) + "-" + year)
+
+               */
+              /*  val date = (dayofMonth.toString() + "-" + (month + 1) + "-" + year)
 
                 val month = ((month + 1))
                 val year = (year)
@@ -90,17 +96,60 @@ class CalendarFragment : Fragment(R.layout.calendar_fragment) {
 
                 binding?.tvCalendar?.text = date
                 viewModel.click.set(true)
-                viewModel.getCalanderDataMonthWise(month, year)*/
+                viewModel.getCalanderDataMonthWise(month, year)*//*
+
+
                 Log.e("DATAEE", clickedDayCalendar.toString())
                 // binding?
+
             }
+
         })
-/*
-        binding?.clCalendar?.setOnDateChangeListener { _, year, month, dayofMonth ->
+*/
+
+        binding?.clCalendar?.setOnPreviousPageChangeListener(object : OnCalendarPageChangeListener {
+            override fun onChange() {
+                month ++
+                var year =2023
+                viewModel.month.set(month)
+                viewModel.year.set(year)
+
+//                binding?.tvCalendar?.text = date
+                viewModel.click.set(true)
+                viewModel.getCalanderDataMonthWise(month, year)
+
+                Log.e("DATAEE", month.toString())
+
+            }
+
+
+        })
+        binding?.clCalendar?.setOnForwardPageChangeListener(object : OnCalendarPageChangeListener {
+            override fun onChange() {
+                 month--
+                var year =2023
+                viewModel.month.set(month)
+                viewModel.year.set(year)
+
+//                binding?.tvCalendar?.text = date
+                viewModel.click.set(true)
+                viewModel.getCalanderDataMonthWise(month , year)
+
+                Log.e("DATAEE", month.toString())
+
+            }
+
+
+        })
+        binding?.clCalendar?.setSelectionBetweenMonthsEnabled(true)
+
+      /*  binding?.clCalendar?.setOnDateChangeListener {
+                _, year, month, dayofMonth ->
             // In this Listener we are getting values
             // such as year, month and day of month
             // on below line we are creating a variable
             // in which we are adding all the variables in it.
+
             val date = (dayofMonth.toString() + "-" + (month + 1) + "-" + year)
 
             val month = ((month + 1))
@@ -117,7 +166,10 @@ class CalendarFragment : Fragment(R.layout.calendar_fragment) {
             // binding?.clCalendar?.dateTextAppearance = Color.RED
 
         }
-*/
+        binding?.clCalendar */
+
+
+
         /* binding?.clCalendar?.setOnDayClickListener(object : OnDayClickListener {
              override fun onDayClick(eventDay: EventDay) {
                  val clickedDayCalendar = eventDay.calendar
